f66f5a6a2a633be90e7deffeb19d83cfa10d3a42
fpa.olarte@gmail.com <fpa.olarte@gmail.com@aa2a1d41-1a9b-82a3-1103-cf1d8a516e89>
2014-06-06 21:26:00 +0000

    Añadida la conexión a la base de datos. Desde acá, todo debería funcionar bien hasta que nos topemos con la comunicación con el dispositivo

---
+++ trunk/transporte_ucv_harvester/ptransporteucv.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v11.5 (32 bit)
+MySQL - 5.6.19-log : Database - ptransporteucv
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ptransporteucv` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ptransporteucv`;
+
+/*Table structure for table `persona` */
+
+DROP TABLE IF EXISTS `persona`;
+
+CREATE TABLE `persona` (
+  `cedula` int(15) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  PRIMARY KEY (`cedula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `persona` */
+
+insert  into `persona`(`cedula`,`nombre`,`apellido`) values (14199311,'maria','olarte'),(17489960,'aura','olarte'),(21536559,'pedro','fernandes');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

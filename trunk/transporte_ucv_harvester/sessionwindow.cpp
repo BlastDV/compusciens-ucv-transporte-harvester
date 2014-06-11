a855be1b8f28d0bdda40a134678220fc158c3d7c
fpa.olarte@gmail.com <fpa.olarte@gmail.com@aa2a1d41-1a9b-82a3-1103-cf1d8a516e89>
2014-06-11 02:43:42 +0000

    Inclusión de la Ventana de Inicio de Sesión. Logica interna de la misma aun no funcional.

---
+++ trunk/transporte_ucv_harvester/sessionwindow.cpp
@@ -0,0 +1,13 @@
+#include "sessionwindow.h"
+#include "ui_sessionwindow.h"
+
+SessionWindow::SessionWindow(QWidget *parent) : QWidget(parent), ui(new Ui::SessionWindow)
+{
+    ui->setupUi(this);
+
+}
+
+SessionWindow::~SessionWindow()
+{
+    delete ui;
+}

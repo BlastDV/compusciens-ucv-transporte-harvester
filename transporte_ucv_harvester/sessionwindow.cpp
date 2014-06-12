93f45687f85bbcb2358b665cfd408d0786fd19c6
fpa.olarte <fpa.olarte@gmail.com>
2014-06-12 19:24:46 +0000

    Se ha agregado la programación para el inicio de sesión, incluido el acceso a la BD. Falta pulir un poco pero ya funciona.

--- transporte_ucv_harvester/sessionwindow.cpp
+++ transporte_ucv_harvester/sessionwindow.cpp
@@ -7,13 +7,71 @@ SessionWindow::SessionWindow(QWidget *parent) : QWidget(parent), ui(new Ui::Sess
 {
     ui->setupUi(this);
 
-    QByteArray result= encrypter->hash("admin", QCryptographicHash::Sha1);
 
-    QMessageBox::critical(0, QObject::tr("Su palabra encriptada"),
-                          QString(result.toHex()));
 }
 
 SessionWindow::~SessionWindow()
 {
     delete ui;
+}
+
+bool SessionWindow::InicioSesion(QString UserID, QString Password)
+{
+    bool INICIOOK; // Para devolver el resultado de la operacion
+
+    // Vamos a crear una conexion para poder autentificar al usuario
+    DBConnector= QSqlDatabase::addDatabase("QODBC");
+    DBConnector.setHostName("localhost");
+    DBConnector.setDatabaseName("ptransporteucv");
+    DBConnector.setUserName("compusciens");
+    DBConnector.setPassword("kakolukiya");
+    DBConnector.setPort(3306);
+
+    if (!DBConnector.open()) // Error en la BD. No disponible?
+    {
+        QMessageBox::critical(0, QObject::tr("Error"),
+        "No se ha podido iniciar sesión, revise el estado"
+        "de la Base de Datos\n\nMensaje: Error DBA1\n"+ DBConnector.lastError().text());
+
+        INICIOOK= false;
+    }
+    else
+    {
+        // Hay que encriptar la clave con SHA1 para compararla con la de la BD
+        QString Encryptedpassword= (Encrypter->hash(QByteArray(Password.toStdString().c_str()), QCryptographicHash::Sha1)).toHex();
+
+        QSqlQuery Loginquery;
+        if (!Loginquery.exec(QString("SELECT * FROM usuarios WHERE id=")+QString("'")+UserID+QString("'")))
+        {
+            QMessageBox::critical(0, QObject::tr("Error"),
+            "No se ha podido iniciar sesión, revise sus datos");
+
+            INICIOOK= false;
+        }
+        else
+        {
+            Loginquery.first();
+
+            // Ahora evaluemos que las claves coinciden
+            if (Loginquery.value(0)!=Encryptedpassword)
+            {
+                QMessageBox::critical(0, QObject::tr("Error"),
+                "No se ha podido iniciar sesión, revise sus datos");
+            }
+            else
+            {
+                QMessageBox::critical(0, QObject::tr("Inicio de Sesión"),
+                "Bienvenido " + UserID);
+            }
+        }
+    }
+
+    DBConnector.close();
+
+    return INICIOOK;
+}
+
+void SessionWindow::on_SessionSubmitButton_clicked()
+{
+    InicioSesion (ui->UserInput->text(), ui->PasswordInput->text());
 }
--- transporte_ucv_harvester/sessionwindow.h
+++ transporte_ucv_harvester/sessionwindow.h
@@ -3,9 +3,11 @@
 
 #include <QWidget>
 #include <QCryptographicHash>
+#include <QtSql/QtSql>
 
-namespace Ui {
-class SessionWindow;
+namespace Ui
+{
+    class SessionWindow;
 }
 
 class SessionWindow : public QWidget
@@ -16,9 +18,15 @@ public:
     explicit SessionWindow(QWidget *parent = 0);
     ~SessionWindow();
 
+private slots:
+    void on_SessionSubmitButton_clicked();
+
 private:
     Ui::SessionWindow *ui;
-    QCryptographicHash *encrypter;
+    QCryptographicHash *Encrypter;
+    QSqlDatabase DBConnector;
+
+    bool InicioSesion (QString UserID, QString Password);
 };
 
 #endif // SESSIONWINDOW_H

c4270c7d284323079bce6073c84f864646bb2bbd
fpa.olarte@gmail.com <fpa.olarte@gmail.com@aa2a1d41-1a9b-82a3-1103-cf1d8a516e89>
2014-06-15 21:25:45 +0000

    Añadidas las clases "DBConnector" y "LogMaster", encargadas de servir como un conector generico para la BD que todos los objetos puedan usar y un objeto que registre cada evento que lleve a cabo un usuario, respectivamente.
    El sistema del Log ya esta en linea y listo para otorgar funcionalidad al resto de los objetos a medida que vayan creciendo y apareciendo.

---
+++ trunk/transporte_ucv_harvester/dbconnector.h
@@ -0,0 +1,26 @@
+#ifndef DBCONNECTOR_H
+#define DBCONNECTOR_H
+
+#include <QObject>
+#include <QtSql>
+
+class DBConnector : public QObject
+{
+    Q_OBJECT
+public:
+    explicit DBConnector(QObject *parent = 0);
+
+    bool RequestConnection ();
+    void EndConnection ();
+    QSqlError getLastError ();
+    
+public slots:
+
+private:
+    QSqlDatabase Connector;
+
+signals:
+    
+};
+
+#endif // DBCONNECTOR_H

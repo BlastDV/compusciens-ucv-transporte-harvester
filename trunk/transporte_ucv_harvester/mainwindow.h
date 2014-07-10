c69f33389a62025f8961e31c62a4661b93212f61
fpa.olarte@gmail.com <fpa.olarte@gmail.com@aa2a1d41-1a9b-82a3-1103-cf1d8a516e89>
2014-07-10 21:36:31 +0000

    Añadidas multiples funcionalidades visuales y logicas en la ventana principal.
    YA SE PUEDEN LEER LSO CODIGOS ALMACENADOS EN EL DISPOSITIVO. LA CLASE Csp32Bridge FUNCIONA!!!
    (ver imagenes del desarrollo en el drive)

--- trunk/transporte_ucv_harvester/mainwindow.h
+++ trunk/transporte_ucv_harvester/mainwindow.h
@@ -6,6 +6,7 @@
 #include "logreporter.h"
 #include "deviceconnector.h"
 #include "dbconnector.h"
+#include "csp32bridge.h"
 
 namespace Ui
 {
@@ -29,7 +30,11 @@ private:
     Ui::MainWindow *ui;
     LogReporter* LogRep;
     DBConnector* Connector;
-    DeviceConnector* DevConnector; // Para la ventana de conexion con el lector
+    DeviceConnector* DevConnector; // Para la ventana de conexion con el lector y la comunicacion
+                                    // con el mismo
+    QMap <QString, int> RouteLocalList; // Esto nos permitira disminuir la cantidad de
+
+    // consultas a la BD al momento de actualizar la lista de paradas dinamicamente
 
     void LoadInitialData(); // Para la ventana de subida de datos inicial
 
@@ -39,8 +44,17 @@ private slots:
 
     void on_actionConectarDispositivo_triggered();
     void DeviceConnectionAborted();
+    void DeviceConnectionAccepted();
     void ReportarMensaje(QString mensaje);
 
+    void UpdateTransportistaC(QString cedula); // Por razones internas de Qt
+    void UpdateTransportistaA(QString apellido);
+    void UpdateRoute(QString id);
+
+    // Esta funcion es la que mas nos interesa
+    void ReadCodes();
+
+    void on_ReadCodesButton_clicked();
 
 signals:
     void CerrarSesion ();
